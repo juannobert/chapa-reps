e2366038d5971dc6016faa42f6affa31ef87ff07
Juan Nobert <nobert407@gmail.com>
2023-01-15 20:44:28 -0300

    Projeto criado

---
+++ src/test/java/com/reps/ChapaRepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChapaRepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
