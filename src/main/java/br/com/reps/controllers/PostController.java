7cd3270a908a381336deb6edcd462e8ff587860b
Juan Nobert <nobert407@gmail.com>
2023-01-16 16:20:39 -0300

    Html da página de posts criado

---
+++ src/main/java/br/com/reps/controllers/PostController.java
@@ -0,0 +1,16 @@
+package br.com.reps.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/posts")
+public class PostController {
+	
+	@GetMapping
+	public String posts() {
+		return "post/notice";
+	}
+
+}
