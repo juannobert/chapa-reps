28d2e5654e86316b4e7fa9fea94b671a5859a89b
Juan Nobert <nobert407@gmail.com>
2023-01-18 15:55:11 -0300

    Salvando posts

--- src/main/java/br/com/reps/services/PostService.java
+++ src/main/java/br/com/reps/services/PostService.java
@@ -5,8 +5,6 @@ import java.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.reps.dtos.requests.PostRequest;
