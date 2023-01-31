b40b31d9d74c3df337b797a74b618cb20fc22b24
Juan Nobert <nobert407@gmail.com>
2023-01-31 18:45:54 -0300

    Entidade Tutorials criada

---
+++ src/main/java/br/com/reps/entities/Tutorials.java
@@ -0,0 +1,31 @@
+package br.com.reps.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_tutorials")
+public class Tutorials {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String title;
+	
+	@Column(nullable = false)
+	private String url;
+	
+	
+}
