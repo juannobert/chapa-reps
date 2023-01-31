a7b673e96efe31fa73919e378393e45adf9a8d90
Juan Nobert <nobert407@gmail.com>
2023-01-31 18:30:46 -0300

    Proteção de rotas implementadas

--- src/main/java/br/com/reps/config/SecurityConfig.java
+++ src/main/java/br/com/reps/config/SecurityConfig.java
@@ -3,13 +3,17 @@ package br.com.reps.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import br.com.reps.entities.enums.UserRole;
 
 @Configuration
 public class SecurityConfig {
@@ -35,7 +39,11 @@ public class SecurityConfig {
 	@Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-        	.anyRequest().permitAll()
+        	.requestMatchers("/auth/cadastro").permitAll() 
+        	.requestMatchers(HttpMethod.POST,"/post/ouvidoria/**").hasAnyAuthority(UserRole.ALUNO.toString())
+        	.requestMatchers(HttpMethod.POST,"/post/**").hasAnyAuthority(UserRole.GREMISTA.toString())
+        	.requestMatchers(HttpMethod.GET,"/post/**").authenticated()
+        	.anyRequest().authenticated()
         	
         	.and()
         	
@@ -58,4 +66,9 @@ public class SecurityConfig {
 
         return http.build();
 	}
+	
+	@Bean
+    WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/resources/**","/h2-console/**");
+    }
 }
