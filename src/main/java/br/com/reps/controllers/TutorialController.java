9f42a6d35db6a67c968239db16509becb9978e32
Juan Nobert <nobert407@gmail.com>
2023-02-12 12:47:09 -0300

    Função de alterar tutoriais implementada

--- src/main/java/br/com/reps/controllers/TutorialController.java
+++ src/main/java/br/com/reps/controllers/TutorialController.java
@@ -2,6 +2,7 @@ package br.com.reps.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.support.Repositories;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -15,6 +16,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.reps.dtos.requests.TutorialRequest;
 import br.com.reps.dtos.responses.AlertMessage;
+import br.com.reps.dtos.responses.TutorialResponse;
 import br.com.reps.services.TutorialService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
@@ -38,6 +40,8 @@ public class TutorialController {
 	public ModelAndView formTutorial() {
 		ModelAndView mv = new ModelAndView("tutorial/form-tutorial");
 		mv.addObject("form", new TutorialRequest());
+		mv.addObject("alter", false);
+
 		
 		return mv;
 	}
@@ -57,5 +61,28 @@ public class TutorialController {
 		service.delete(id);
 		attrs.addFlashAttribute("alert",new AlertMessage("Tutorial excluido com sucesso","alert-primary"));
 		return "redirect:/tutoriais";
+	}
+	
+	@GetMapping("/alterar/{id}")
+	public ModelAndView alterar(@PathVariable Long id) {
+		ModelAndView mv = new ModelAndView("tutorial/form-tutorial");
+		TutorialResponse response = service.findByid(id);
+		response.setUrl(service.toLink(response.getUrl()));
+		mv.addObject("form", response);
+		mv.addObject("alter", true);
+		
+		return mv;
+	}
+	
+	@PostMapping("/alterar/{id}")
+	public String alterar(@Valid @ModelAttribute("form") @PathVariable Long id, TutorialRequest request,BindingResult result,RedirectAttributes attrs) {
+		if(result.hasErrors()) {
+			return "tutorial/form-tutorial";
+		}
+		
+		service.alter(id, request);
+		attrs.addFlashAttribute("alert",new AlertMessage("Tutorial alterado com sucesso","alert-primary"));
+		return "redirect:/tutoriais";
+		
 	}
 }
--- src/main/java/br/com/reps/services/TutorialService.java
+++ src/main/java/br/com/reps/services/TutorialService.java
@@ -13,6 +13,7 @@ import br.com.reps.entities.Tutorial;
 import br.com.reps.entities.User;
 import br.com.reps.mappers.TutorialMapper;
 import br.com.reps.repositories.TutorialRepository;
+import br.com.reps.services.exceptions.EntityNotFoundException;
 import br.com.reps.utils.SecurityUtils;
 import jakarta.transaction.Transactional;
 
@@ -35,8 +36,7 @@ public class TutorialService {
 	
 	@Transactional
 	public Tutorial insert(TutorialRequest request) {
-		String youtubeLink = request.getUrl();
-		String embedLink = youtubeLink.replace("youtu.be/", "www.youtube.com/embed/");
+		String embedLink = toEmbedLink(request.getUrl());
 		Tutorial entity = mapper.toModel(request);
 		User author = securityUtils.getUsuarioLogado(); 
 		entity.setDate(new Date());
@@ -47,7 +47,28 @@ public class TutorialService {
 		return repository.save(entity);
 	}
 	
+	public TutorialResponse findByid(Long id) {
+		return repository.findById(id)
+				.map(mapper::toResponse)
+				.orElseThrow(() -> new EntityNotFoundException("Tutorial não encontrado"));
+	}
+	
+	public Tutorial alter(Long id,TutorialRequest request) {
+		Tutorial model = repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Tutorial não encontrado"));
+		model.setTitle(request.getTitle());
+		model.setUrl(toEmbedLink(request.getUrl()));
+		return repository.save(model);
+	}
+	
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+	
+	private String toEmbedLink(String link) {
+		return link.replace("youtu.be/", "www.youtube.com/embed/");
+	}
+	
+	public String toLink(String embedLink) {
+		return embedLink.replace("www.youtube.com/embed/","youtu.be/");
 	}
 }
