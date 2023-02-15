605ab646e02fcbe7427e481a371707bc14d4b8ea
Juan Nobert <nobert407@gmail.com>
2023-02-15 12:35:11 -0300

    Java Mail Sender adicionado e Email Service criado

---
+++ src/main/java/br/com/reps/services/EmailService.java
@@ -0,0 +1,15 @@
+package br.com.reps.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	
+	
+}
