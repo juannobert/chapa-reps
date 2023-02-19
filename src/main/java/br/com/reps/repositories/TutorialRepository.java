740c2812cb718cfc5ca0cfeac10dad0dc2dc5d81
Juan Nobert <nobert407@gmail.com>
2023-02-19 12:27:21 -0300

    Ordenando tutoriais por id

--- src/main/java/br/com/reps/repositories/TutorialRepository.java
+++ src/main/java/br/com/reps/repositories/TutorialRepository.java
@@ -1,10 +1,14 @@
 package br.com.reps.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.reps.entities.Tutorial;
 
 public interface TutorialRepository extends JpaRepository<Tutorial, Long>{
 	
-	
+	@Query("SELECT t  FROM Tutorial t ORDER BY t.id DESC")
+	Page<Tutorial> findAll(Pageable pageable);
 }
